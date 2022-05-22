1409807d0d043cac0fc8bca5d3e56b68e45893d6
namdevpuri3@gmail.com <namdevpuri3@gmail.com>
2022-05-22 21:42:39 +0530

    final

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -24,16 +24,10 @@ const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
-
-
-
-// const isValid= function(value){
-//     if(typeof (value)==='undefined' || typeof (value)==='null') return false
-//     if(typeof (value) === 'string' && value.trim().length > 0) return false
-//     return true
-// }
-const isValidRequestBody = function (requestBody) {
-    return Object.keys(requestBody).length > 0
+const isValid= function(value){
+    if(typeof (value)==='undefined' || typeof (value)==='null') return false
+    if(typeof (value) === 'string' && value.trim().length > 0) return false
+    return true
 }
 
 
@@ -42,93 +36,89 @@ const baseUrl= 'http://localhost:3000'
 //*********************************************// Short Url //************************************************* */
 
 const shortUrl = async function ( req, res ) {
-    try{
-    const Url = req.body
-    // console.log(longUrl)
-    const longUrl =Url.longUrl
-    if (!isValidRequestBody({longUrl})) {
-        return res.status(400).send({status:false , msg:" please enter longUrl details"})
-    }    
-    if (!validUrl.isUri(longUrl)) {
-        return res.status(400).send({status:false,msg:'Invalid long URL'})
-    }
-    if(!validUrl.isUri(baseUrl)){
-        return res.status(400).send({status:false, msg:'The base URL is invalid'})
-    }
-    //checking url in cache server memory
-    const dataCache = await GET_ASYNC(`${longUrl}`)
-    if(dataCache){
-        return res.status(200).send({status: true, data: JSON.parse(dataCache)})
-    }
-    else{
+    let longUrl = req.body.longUrl
+    const urlCode = shortid.generate()
 
+    if(longUrl){
+    try{
+        longUrl = longUrl.trim()
+
+        if(!(longUrl.includes('//'))){
+            return res.status(400).send({status:false, msg:"Invalid longUrl"})
+        }
+        const urlParts= longUrl.split("//")
+        const scheme = urlParts[0]
+        const uri = urlParts[1]
+        let islongUrl
+
+        if(!(uri.includes("."))){
+            return res.status(400).send({status:false, msg:"Invalid longUrl"})
+        }
+        const uriParts = uri.split(".")
+
+        if(!(((scheme == "https:") || (scheme == "http:") && (uriParts[0].trim().length) && (uriParts[1].trim().length)))){
+            return res.status(400).send({status:false, msg:'Invalid longUrl'})
+        }
          // db call
-    const islongUrl= await urlModel.findOne({longUrl}).select({_id:0, longUrl:1, shortUrl:1, urlCode:1 })
+     islongUrl= await urlModel.findOne({longUrl})//.select({_id:0, longUrl:1, shortUrl:1, urlCode:1 })
     if(islongUrl){
 
-        await SET_ASYNC(`${longUrl}`, JSON.stringify(islongUrl))
-        return res.status(200).send({status:true, message:"get from DB", data:islongUrl})
-    }
+        return res.status(200).send({status:true, data:islongUrl})
 
-    // Generate :urlCode
-    const urlCode = shortid.generate(longUrl)
+    } else {
+
+     const shortUrl = baseUrl + '/' + urlCode.toLowerCase()
+
+        const data= {
+            longUrl: longUrl,
+            shortUrl: shortUrl,
+            urlCode: urlCode, 
+          }
+
+        const createUrl=await urlModel.create(data)
+
+        await SET_ASYNC(urlCode.toLowerCase(),longUrl)
+
+        return res.status(201).send({msg:"you already created shortUrl for this longUrl", data:createUrl})
     
-    let isUrlCode = await urlModel.findOne({urlCode:urlCode})
-    if(isUrlCode){
-        return res.status(400).send({status:false,message:"already exist"})
     }
-// create: short URL
-    const shortUrl = baseUrl + '/' + urlCode
-// create: doc for database including(urlCode, shortUrl, longUrl)
-    const data= {
-      longUrl: longUrl,
-      shortUrl: shortUrl,
-      urlCode: urlCode, 
-    }
-    // creating: document
-    const createUrl=await urlModel.create(data)
 
-    await SET_ASYNC(`${longUrl}`, JSON.stringify(data))
-    await SET_ASYNC(`${longUrl}`, JSON.stringify(data.longUrl))
-
-    return res.status(201).send({msg:"you already created shortUrl for this longUrl", data:createUrl})
+}  
+catch (err){
+   res.status(500).send({status:false, msg:err.massage})
 }
-} catch (err){
-    res.status(500).send({status:false, msg:err.massage})
-}
+   } else {
+       return res.status(400).send({status:false , msg:" please enter longUrl details"})
+   }
 }
 
 //**********************************************// getlink //**************************************************** */
 
 const getShortUrl= async function (req,res){
     try{
-    let urlCode=req.params.urlCode.trim()
+     let urlCode=req.params.urlCode.trim()
 
-    if (!isValidRequestBody(urlCode)) {
-        return res.status(400).send({status:false, msg:'Please enter urlCode in the path params'})
-    }
     //checking url in cache server memory
-    let getDataFromCache = await GET_ASYNC(`$(req.params.urlCode)`)
-    if(getDataFromCache){
+    let cachedLongUrl = await GET_ASYNC(urlCode)
+    if(cachedLongUrl){
+       // console.log("cachedLongUrl=",cachedLongUrl )
 
-    const data = JSON.parse(getDataFromCache)
-       res.status(302).redirect(data.longUrl)
-       return
+      return res.status(302).redirect(cachedLongUrl)
+       
     }
     else {
         // db call
-      const url = await urlModel.findOne({urlCode:urlCode}).select({_id:0, urlCode:1, shortUrl:1, longUrl:1})
+      const url = await urlModel.findOne({urlCode:req.params.urlCode})
   
-    if(!url) return res.status(404).send({status:false,message:"urlcode not found"})
-
+    if(url) {
     const getlongUrl = url.longUrl
-
-     await SET_ASYNC(`${urlCode}`, JSON.stringify(`${getlongUrl}`))
-     
      return res.status(302).redirect(getlongUrl) // 302 stand for successful redirection
     
+    }else{
+        return res.status(404).send({status:false,message:"urlcode not found"})
+
     }
-    
+} 
 } catch(err) {   
     console.log(err);
     res.status(500).send({status:false, msg:err.massage})
